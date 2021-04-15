800a2cec25d8578e4cd2720b51291a631f0abca9
Erik Ruotsalainen <44022872+shved-nmr@users.noreply.github.com>
2021-04-15 17:21:01 +0200

    Set up rand(), added global score, score logic and result checker

--- main.cpp
+++ main.cpp
@@ -4,25 +4,50 @@
 #include <ctime>
 using namespace std;
 
+int pl1_score = 0;
+int com_score = 0;
+
 int rand_num(){
-    srand(time(0));
-    int n = (rand() % 6 + 1);
+    //srand(time(0));
+    int n = rand() % 6 + 1;
     return n;
+}
+int print_score(){
+    cout << "\n The score is:\nPLayer: " << pl1_score << "\tComputer: " << com_score;
+    return 0;
+}
+string who_won(int a, int b){
+    if (a>b){
+        return "W";
+    } else if (a<b) {
+        return "L";
+    } else {
+        return "D";
+    }
 }
 int game_play(){
     int pl1_num = 0;
     int com_num = 0;
 
-    pl1_num = rand_num();
-    com_num = rand_num();
+    while(pl1_score < 5 && com_score < 5) {
+        pl1_num = rand_num();
+        com_num = rand_num();
 
-    if(pl1_num > com_num){
-        cout << "Player wins this round!";
-    } else if (pl1_num == com_num){
-        cout << "It's a draw!";
-    } else{
-        cout << "Computer wins this round!";
+        if (who_won(pl1_num, com_num) == "W") {
+            pl1_score++;
+            cout << "\nPlayer wins this round!";
+            print_score();
+        } else if (who_won(pl1_num, com_num) == "L") {
+            com_score++;
+            cout << "\nComputer wins this round!";
+            print_score();
+        } else {
+            cout << "\nIt's a draw!";
+            print_score();
+        }
     }
+
+    return 0;
 }
 
 int main() {
